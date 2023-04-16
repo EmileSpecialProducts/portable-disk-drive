24b48205fc8da82761379d28ba1f03601caf001a
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2023-04-16 16:28:34 +0200

    Initial version that supports the ESP8266, ESP32,  ESP32S2 and ESP32C3 ( not test yet )

--- SDWebServer/SDWebServer.ino
+++ SDWebServer/SDWebServer.ino
@@ -27,21 +27,191 @@
   upload the contents of SdRoot to the root of the SDcard and access the editor by going to http://esp8266sd.local/edit
 
 */
+
+#include <Arduino.h>        //  https://github.com/espressif/arduino-esp32/tree/master/cores/esp32
+
+#if defined(ESP8266)
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#else
+#include <WiFi.h>           // https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFi
+#include <WebServer.h>      // https://github.com/espressif/arduino-esp32/tree/master/libraries/WebServer
+#include <ESPmDNS.h>        // https://github.com/espressif/arduino-esp32/tree/master/libraries/ESPmDNS
+#endif
+
+#include <WiFiClient.h>     // https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFi
+#include <NTPClient.h>      //  https://github.com/arduino-libraries/NTPClient
+#include <WiFiManager.h>    // WifiManager by tzapu  https://github.com/tzapu/WiFiManager
+#include <ArduinoOTA.h>     // ArduinoOTA by Arduino, Juraj  https://github.com/JAndrassy/ArduinoOTA
+
 #include <SPI.h>
 #include <SD.h>
+#include "NTP.hpp"
+
 
 #define DBG_OUTPUT_PORT Serial
+#if defined(ESP8266)
+#warning "ESP8266 Pins You can not change them"
+//WeMos D1 Mini pinout
+//Label GPIO    Input        Output Notes
+//RTS   RESET 
+//A0    ADC0    Analog Input X
+//D0    GPIO16  no interrupt no     PWM or I2C support HIGH at boot used to wake up from deep sleep
+//D5    GPIO14  OK           OK     SPI (SCLK)
+//D6    GPIO12  OK           OK     SPI (MISO)
+//D7    GPIO13  OK           OK     SPI (MOSI)
+//D8    GPIO15  pulled toGND OK     SPI (CS) Boot fails if pulled HIGH
+//3V3 
 
-const char* ssid = "NETGEAR94";
-const char* password = "1234567890";
-const char* host = "esp8266sd";
+//TX    GPIO1   TX pin       OK     HIGH at boot debug output at boot, boot fails if pulled LOW
+//RX    GPIO3   OK           RX pin HIGH at boot
+//D1    GPIO5   OK           OK     often used as SCL (I2C)
+//D2    GPIO4   OK           OK     often used as SDA (I2C)
+//D3    GPIO0   pulled up    OK     connected to FLASH button, boot fails if pulled LOW
+//D4    GPIO2   pulled up    OK     HIGH at boot connected to on-board LED, boot fails if pulled LOW
+//GND
+//5V
 
+#define SD_PIN_SCK         14 //D5 GPIO14 WeMos D1 Mini 
+#define SD_PIN_MOSI        13 //D7 GPIO13 WeMos D1 Mini 
+#define SD_PIN_MISO        12 //D6 GPIO12 WeMos D1 Mini 
+#define SD_PIN_CS          15 //D8 GPIO15 WeMos D1 Mini 
+#define BOOTPIN            16 //D0 GPIO16 WeMos D1 Mini
+
+#define LED_RED_PIN        2  //D4 GPIO2 WeMos D1 Mini on-board LED
+#define LED_ORANGE_PIN     5  //D1 GPIO5 WeMos D1 Mini 
+#define LED_GREEN_PIN      4  //D2 GPIO4 WeMos D1 Mini 
+
+#define RXD               3
+#define TXD               1
+
+#elif defined(CONFIG_IDF_TARGET_ESP32S2)
+#warning "TARGET_ESP32S2"
+/*
+https://deguez07.medium.com/esp32-with-sd-card-modules-the-master-guide-5d391f6785d7
+https://github.com/Xinyuan-LilyGO/LilyGo-esp32s2-base
+https://github.com/Xinyuan-LilyGO/ESP32_S2
+*/
+
+#define SD_PIN_SCK         12
+#define SD_PIN_MOSI        11
+#define SD_PIN_MISO        13
+#define SD_PIN_CS          10
+#define BOOTPIN            0
+
+#define LED_RED_PIN        2
+#define LED_ORANGE_PIN     3
+#define LED_GREEN_PIN      4
+
+#define RXD               40
+#define TXD               39
+
+#elif defined(CONFIG_IDF_TARGET_ESP32C3)
+
+#define SD_PIN_SCK         3
+#define SD_PIN_MOSI        4
+#define SD_PIN_MISO        5
+#define SD_PIN_CS          10
+#define BOOTPIN            9
+
+#define LED_RED_PIN        2
+#define LED_ORANGE_PIN     6
+#define LED_GREEN_PIN      7
+
+#define RXD               20
+#define TXD               21
+
+
+#elif defined(CONFIG_IDF_TARGET_ESP32)
+/****************************************************************************
+    pin setup
+    Left
+    1  - 3.3 V                                        1
+    2  - EN - RESET                                   2
+    3  - GPIO36 - ADC0  - INPUT ONLY                  3
+    4  - GPIO39 - ADC3  - INPUT ONLY                  4
+    5  - GPIO34 - ADC6  - INPUT ONLY                  5
+    6  - GPIO35 - ADC7  - INPUT ONLY                  6
+    7  - GPIO32 - ADC4  - TOUCH9                      7  
+    8  - GPIO33 - ADC5  - TOUCH8                      8  
+    9  - GPIO25 - ADC18 - DAC1                        9  
+    10 - GPIO26 - ADC19 - DAC2                        10 
+    11 - GPIO27 - ADC17 - TOUCH7                      11
+    12 - GPIO14 - ADC16 - TOUCH6 - SPICLK             12
+    13 - GPIO12 - ADC15 - TOUCH5 - SPIMISO            13
+    14 - GND                                          14 - GND
+    15 - GPIO13 - ADC14 - TOUCH4 - SPIMOSI            15
+    16 - GPIO9  - RX1 - Do not USE Connected to Flash 16
+    17 - GPIO10 - TX1 - Do not USE Connected to Flash 17
+    18 - GPIO11       - Do not USE Connected to Flash 18
+    19 - Vin                                          19
+
+    1  - GND                                          1  - GND
+    2  - GPIO23 - V SPIMOSI                           2  - MOSI
+    3  - GPIO22 - I2C SCL                             3  
+    4  - TX0    - GPIO1                               4  - USB-TX
+    5  - RX0    - GPIO3                               5  - USB-RX
+    6  - GPIO21 - I2CSDA                              6   
+    7  - GND                                          7  - GND
+    8  - GPIO19 - V SPIMISO                           8  - MISO
+    9  - GPIO18 - V SPISCK                            9  - SCK
+    10 - GPIO5  - V SPI SS                            10 - CSN
+    11 - GPIO17 - TX2                                 11
+    12 - GPIO16 - RX2                                 12
+    13 - GPIO4  - ADC10 - TOUCH0                      13 - 
+    14 - GPIO0  - ADC11 - TOUCH1 - BOOT               14
+    15 - GPIO2  - ADC12 - TOUCH2 - OnBoardLed         15 -- LEDPIN
+    16 - GPIO15 - ADC13 - TOUCH3 - SPISS              16 
+    17 - GPIO8  - Do not USE Connected to Flash       17
+    18 - GPIO7  - Do not USE Connected to Flash       18
+    19 - GPIO6  - Do not USE Connected to Flash       19
+
+    GPIO2 = Debug led on Board LOW = OFF        HIGH = on
+*/
+
+#define SD_PIN_SCK         18
+#define SD_PIN_MOSI        23
+#define SD_PIN_MISO        19
+#define SD_PIN_CS          5
+#define BOOTPIN            0
+
+#define LED_RED_PIN        15
+#define LED_ORANGE_PIN     2
+#define LED_GREEN_PIN      32
+
+#define RXD               3
+#define TXD               1
+
+#endif
+
+#define USEWIFIMANAGER 
+#ifndef USEWIFIMANAGER
+const char* ssid = "SSID_ROUTER";
+const char* password = "Password_Router";
+#endif
+
+const char* host = "ESP-NAS";
+
+#if defined(ESP8266) 
 ESP8266WebServer server(80);
+#else
+WebServer server(80);
+#endif
 
+#if not defined(ESP8266) || not defined(CONFIG_IDF_TARGET_ESP32)
+SPIClass * sd_spi = NULL;
+#endif
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+unsigned long PreviousTimeSeconds;
+unsigned long PreviousTimeMinutes;
+unsigned long PreviousTimeHours;
+unsigned long PreviousTimeDay;
+uint16_t Config_Reset_Counter=0;
+int OTAUploadBusy=0;
 static bool hasSD = false;
 File uploadFile;
 
@@ -155,7 +325,7 @@ void handleCreate(){
   if(path.indexOf('.') > 0){
     File file = SD.open((char *)path.c_str(), FILE_WRITE);
     if(file){
-      file.write((const char *)0);
+      file.write(0);
       file.close();
     }
   } else {
@@ -220,16 +390,50 @@ void handleNotFound(){
 }
 
 void setup(void){
+  pinMode(BOOTPIN, INPUT_PULLUP);
+  pinMode(LED_RED_PIN, OUTPUT);
+#if defined(ESP8266) 
   DBG_OUTPUT_PORT.begin(115200);
+#else
+  DBG_OUTPUT_PORT.begin(115200,  SERIAL_8N1, RXD, TXD);
+#endif 
   DBG_OUTPUT_PORT.setDebugOutput(true);
   DBG_OUTPUT_PORT.print("\n");
+#ifdef USEWIFIMANAGER
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    //  wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+     res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect(DeviceName); // anonymous ap
+    // res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+    if(!res) {
+        DBG_OUTPUT_PORT.println("Failed to connect Restarting");
+        delay(5000);
+         if (digitalRead(BOOTPIN)==LOW){ 
+          //wm.resetSettings();
+        }
+        ESP.restart();
+    }
+    else {
+        //if you get here you have connected to the WiFi    
+    }
+#else
   WiFi.begin(ssid, password);
   DBG_OUTPUT_PORT.print("Connecting to ");
   DBG_OUTPUT_PORT.println(ssid);
-
   // Wait for connection
   uint8_t i = 0;
-  while (WiFi.status() != WL_CONNECTED && i++ < 20) {//wait 10 seconds
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) { //wait 10 seconds
     delay(500);
   }
   if(i == 21){
@@ -237,8 +441,11 @@ void setup(void){
     DBG_OUTPUT_PORT.println(ssid);
     while(1) delay(500);
   }
+#endif  
   DBG_OUTPUT_PORT.print("Connected! IP address: ");
   DBG_OUTPUT_PORT.println(WiFi.localIP());
+  DBG_OUTPUT_PORT.print("Connecting to ");
+  DBG_OUTPUT_PORT.println(WiFi.SSID());
 
   if (MDNS.begin(host)) {
     MDNS.addService("http", "tcp", 80);
@@ -248,6 +455,52 @@ void setup(void){
     DBG_OUTPUT_PORT.println(".local");
   }
 
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    OTAUploadBusy=60; // only do a update for 60 sec;
+    DBG_OUTPUT_PORT.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    OTAUploadBusy=0;
+    DBG_OUTPUT_PORT.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    DBG_OUTPUT_PORT.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    OTAUploadBusy=0;
+    DBG_OUTPUT_PORT.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      DBG_OUTPUT_PORT.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      DBG_OUTPUT_PORT.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      DBG_OUTPUT_PORT.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      DBG_OUTPUT_PORT.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      DBG_OUTPUT_PORT.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
   server.on("/list", HTTP_GET, printDirectory);
   server.on("/edit", HTTP_DELETE, handleDelete);
@@ -258,12 +511,114 @@ void setup(void){
   server.begin();
   DBG_OUTPUT_PORT.println("HTTP server started");
 
-  if (SD.begin(SS)){
+#if defined(ESP8266) || defined(CONFIG_IDF_TARGET_ESP32)
+  if (SD.begin(SD_PIN_CS)){
      DBG_OUTPUT_PORT.println("SD Card initialized.");
      hasSD = true;
   }
+#else
+  sd_spi = new SPIClass(SPI_MODE1);
+  sd_spi->begin(SD_PIN_SCK,SD_PIN_MISO,SD_PIN_MOSI,SD_PIN_CS);
+  pinMode(sd_spi->pinSS(), OUTPUT);
+  sd_spi->begin(SD_PIN_CS, 25000000);
+  if (SD.begin(SD_PIN_CS, *sd_spi)){
+     DBG_OUTPUT_PORT.println("SD Card initialized.");
+     hasSD = true;
+  }
+#endif
+
+  timeClient.begin();
+  timeClient.setUpdateInterval(1000 * 60 * 60 * 24);  // 24 uur
+  timeClient.update();
+
+  if (SD.exists("/Log.txt")){
+    String message = "Reboot from: ";
+#if defined(ESP8266) 
+    File LogFile = SD.open("/Log.txt",FILE_WRITE);
+#else
+    File LogFile = SD.open("/Log.txt",FILE_APPEND);
+#endif
+    message += " ESPDate: " +String( getFormattedDateTime(timeClient.getEpochTime()));
+
+#if defined(ESP8266) 
+    message +=  ESP.getChipId();
+#else
+    message +=  ESP.getChipModel();
+#endif
+    message += "_";
+    message += WiFi.macAddress(); 
+    message += " LocalIpAddres: "+ WiFi.localIP().toString();
+    message += " SSID: " +String(WiFi.SSID());
+    message += " Rssi: " +String(WiFi.RSSI());
+
+#if not defined(ESP8266) 
+    message += " Total heap: " +String( ESP.getHeapSize()  / 1024);
+    message += " Free heap: "  +String( ESP.getFreeHeap()  / 1024);
+    message += " Total PSRAM: "+String( ESP.getPsramSize() / 1024);
+    message += " Free PSRAM: " +String( ESP.getFreePsram() / 1024);
+    message += " Temperature: "+String(temperatureRead())+" °C "; // internal TemperatureSensor
+#if defined(CONFIG_IDF_TARGET_ESP32)
+    message += " HallSensor: " +String(hallRead());
+#endif
+    message += " FlashChipMode: "+String( ESP.getFlashChipMode());
+#else
+    message += " FlashChipId: "+String( ESP.getFlashChipId());
+    message += " FlashChipRealSize: "+String( ESP.getFlashChipRealSize());
+#endif 
+    message += " FlashChipSize: "+String( ESP.getFlashChipSize());
+    message += " FlashChipSpeed: "+String( ESP.getFlashChipSpeed());
+    message += " FlashChipMode: "+String( ESP.getFlashChipMode());
+
+    message += " Build Date: "+ String (__DATE__ " " __TIME__);
+    LogFile.println(message);
+  }
+
 }
 
 void loop(void){
-  server.handleClient();
+  unsigned long currentTimeSeconds=timeClient.getEpochTime();
+  yield();
+  ArduinoOTA.handle();
+  if(OTAUploadBusy ==0 )
+  { // Do not do things that take time when OTA is busy
+    server.handleClient();
+  }
+
+  if(PreviousTimeDay != (currentTimeSeconds/(60*60*24)))
+  {   // Day Loop 
+    PreviousTimeDay = (currentTimeSeconds/(60*60*24));
+    timeClient.update();
+  }
+  if(PreviousTimeHours != (currentTimeSeconds/(60*60)))
+  {   // Hours Loop
+    PreviousTimeHours = (currentTimeSeconds/(60*60));
+  }
+
+  if(PreviousTimeMinutes != (currentTimeSeconds/60))
+  {   // Minutes loop
+    PreviousTimeMinutes = (currentTimeSeconds/60);
+    if ((WiFi.status() != WL_CONNECTED)) { // if WiFi is down, try reconnecting
+      WiFi.disconnect();
+      WiFi.reconnect();
+    }
+  }
+
+  if(PreviousTimeSeconds != currentTimeSeconds)
+  { // 1 second loop
+    PreviousTimeSeconds = currentTimeSeconds;
+      if(digitalRead(LED_RED_PIN )==0)   digitalWrite(LED_RED_PIN, 1); else   digitalWrite(LED_RED_PIN, 0);
+      if(OTAUploadBusy >0 ) OTAUploadBusy--;
+#ifdef USEWIFIMANAGER      
+      if (digitalRead(BOOTPIN)==LOW){     
+        if( ++Config_Reset_Counter>5 ){ // press the BOOT 5 sec to reset the WifiManager Settings
+          WiFiManager wm;               //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+          delay(500);
+          wm.resetSettings();
+          ESP.restart();
+        } 
+      } else {
+        Config_Reset_Counter=0;
+      }
+#endif
+  }  
 }
